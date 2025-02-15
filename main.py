ffd872c4d668b4dac7b743e64f5bd9fe9281b485
MrJam-python <jmccabe-tcspas@outlook.com>
2025-02-15 11:02:21 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 import random
-print ("------------------------------------------------------------------------------------------------------------------------------")
+print ("-------------------------------------------------------------")
 print ("Welcome To Rock, Paper, Scissors!")
-print ("------------------------------------------------------------------------------------------------------------------------------")
-player_choice=input ("Choose Rock, Paper, Or Scissors.")
-cpu_choice= random.randint(1,3)
-if player_choice=="rock" and cpu_choice== 2:
-    print ("You Lost!")
+print ("-------------------------------------------------------------")
+
+
+
+while True:
+    player_choice=input ("Choose Rock, Paper, Or Scissors.")
+    cpu_choice= random.choice(["rock","paper","scissors"])
+    print("You picked " + player_choice)
+    print("The computer picked " + cpu_choice)
+
+    if player_choice == cpu_choice:
+        print("You Tied!") 
+     
+    if player_choice=="rock" and cpu_choice== "paper":
+        print("You Lost!")
+    
+    if player_choice=="rock" and cpu_choice== "scissors":
+        print("You Won!")
+    
+    if player_choice=="paper" and cpu_choice== "scissors":
+        print("You Lost!")
+    
+    if player_choice=="paper" and cpu_choice== "rock":
+        print("You Won!")
+    
+    if player_choice=="scissors" and cpu_choice== "rock":
+        print("You Lost!")
+    
+    if player_choice=="scissors" and cpu_choice== "paper":
+        print("You Won!")
