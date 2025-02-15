846622c483c5b4deb3896b9c0570c012f35f81ef
MrJam-python <jmccabe-tcspas@outlook.com>
2025-02-15 10:23:14 -0800

    notes

---
+++ notes.py
@@ -0,0 +1,10 @@
+# interpreter
+# this variable stores an integer value 3
+number=3
+print (number)
+# this variable stores a string value called "James"
+variable="James"
+# this variable stores a boolean value False
+variable=False
+variable= 3*3
+print(variable)
