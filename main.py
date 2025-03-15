1e813a45d3be3e08057a416f2021b824159720fa
MrJam-python <jmccabe-tcspas@outlook.com>
2025-03-15 10:58:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -12,31 +12,48 @@ while counter < 3:
     cpu_choice= random.choice(["rock","paper","scissors"])
     print("You picked " + player_choice)
     print("The computer picked " + cpu_choice)
-
-    if player_choice == cpu_choice:
-        print("You Tied!") 
+    if player_choice != "rock" and player_choice != "paper" and player_choice != "scissors":
+        print("that option is not avaliable. Please choose another option.")
+        counter -= 1
+    
+    elif player_choice == cpu_choice:
+        print("You Tied This Round!")
+       
      
     elif player_choice=="rock" and cpu_choice== "paper":
-        print("You Lost!")
+        print("You Lost The Round!")
         cpu_won+=1
+        
     
     elif player_choice=="rock" and cpu_choice== "scissors":
         print("You Won The Round!")
         player_won+=1
+        
     
     elif player_choice=="paper" and cpu_choice== "scissors":
-        print("You Lost!")
+        print("You Lost The Round!")
         cpu_won+=1
     
     elif player_choice=="paper" and cpu_choice== "rock":
-        print("You Won!")
+        print("You Won The Round!")
         player_won+=1
     
     elif player_choice=="scissors" and cpu_choice== "rock":
-        print("You Lost!")
+        print("You Lost The Round!")
         cpu_won+=1
     
     elif player_choice=="scissors" and cpu_choice== "paper":
-        print("You Won!")
+        print("You Won The Round!")
         player_won+=1
     counter += 1
+    
+print ("-------------------------------------------------------------")
+print("The player won " + str(player_won) + " times!")
+print("The computer won " + str(cpu_won) + " times!")
+print ("-------------------------------------------------------------")
+if player_won > cpu_won:
+    print("The player won the game!")
+elif player_won < cpu_won:
+    print("The cpu won the game!")
+elif player_won == cpu_won:
+    print("The game resulted in a tie!")
