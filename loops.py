308fd6bab38bdc14122ce76e29e15c36df382e36
MrJam-python <jmccabe-tcspas@outlook.com>
2025-03-22 11:00:45 -0700

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,10 @@
+
+item_num=2
+  
+
+names = ["James" , "John" , "Regie" ]
+#          0         1        2
+#          -3        -2       -1 
+while item_num >= 0:
+    print (names[item_num])
+    item_num -= 1
